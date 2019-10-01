4ff77f82dcd6f4def07a6f7f9ad0d64dd0b61be7
Stéphane Meny <smeny@users.noreply.github.com>
2019-10-01 14:04:36 +0100

    Initial commit

---
+++ src/main/java/com/github/smeny/GildedRose.java
@@ -0,0 +1,65 @@
+package com.github.smeny;
+
+class GildedRose {
+
+    private static final String BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
+
+    Item[] items;
+
+    public GildedRose(Item[] items) {
+        this.items = items;
+    }
+
+    public void updateQuality() {
+        for (Item item : items) {
+            if (!item.name.equals("Aged Brie")
+                    && !item.name.equals(BACKSTAGE_PASS)) {
+                if (item.quality > 0) {
+                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        item.quality = item.quality - 1;
+                    }
+                }
+            } else {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+
+                    if (item.name.equals(BACKSTAGE_PASS)) {
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
+                            }
+                        }
+
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                item.sellIn = item.sellIn - 1;
+            }
+
+            if (item.sellIn < 0) {
+                if (!item.name.equals("Aged Brie")) {
+                    if (!item.name.equals(BACKSTAGE_PASS)) {
+                        if (item.quality > 0) {
+                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                                item.quality = item.quality - 1;
+                            }
+                        }
+                    } else {
+                        item.quality = 0;
+                    }
+                } else {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/github/smeny/Item.java
@@ -0,0 +1,21 @@
+package com.github.smeny;
+
+public class Item {
+
+    String name;
+
+    int sellIn;
+
+    int quality;
+
+    public Item(String name, int sellIn, int quality) {
+        this.name = name;
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + ", " + this.sellIn + ", " + this.quality;
+    }
+}
