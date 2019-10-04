e7f5d87e8940290ba1893d6a5589b4c269e7e6f7
Stéphane Meny <smeny@users.noreply.github.com>
2019-10-04 12:31:11 +0100

    Enough tests to cover past requirements

--- src/main/java/com/github/smeny/GildedRose.java
+++ src/main/java/com/github/smeny/GildedRose.java
@@ -2,7 +2,12 @@ package com.github.smeny;
 
 class GildedRose {
 
-    private static final String BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
+    static final String BACKSTAGE_PASS_NAME = "Backstage passes to a TAFKAL80ETC concert";
+    static final String AGED_BRIE_NAME = "Aged Brie";
+    static final String SULFURAS_NAME = "Sulfuras, Hand of Ragnaros";
+    static final int SULFURAS_QUALITY = 80;
+    static final int ITEM_MINIMUM_QUALITY = 0;
+    static final int ITEM_MAXIMUM_QUALITY = 50;
 
     Item[] items;
 
@@ -12,42 +17,33 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals("Aged Brie")
-                    && !item.name.equals(BACKSTAGE_PASS)) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
-                }
+            if (!item.name.equals(AGED_BRIE_NAME) && !item.name.equals(BACKSTAGE_PASS_NAME)) {
+                updateCommonItemQuality(item);
             } else {
-                if (item.quality < 50) {
+                if (item.quality < ITEM_MAXIMUM_QUALITY) {
                     item.quality = item.quality + 1;
 
-                    if (item.name.equals(BACKSTAGE_PASS)) {
+                    if (item.name.equals(BACKSTAGE_PASS_NAME)) {
                         if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
+                            increaseItemQualityIfUnderMaximum(item, 1);
                         }
 
                         if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
+                            increaseItemQualityIfUnderMaximum(item, 1);
                         }
                     }
                 }
             }
 
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!item.name.equals(SULFURAS_NAME)) {
                 item.sellIn = item.sellIn - 1;
             }
 
             if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals(BACKSTAGE_PASS)) {
+                if (!item.name.equals(AGED_BRIE_NAME)) {
+                    if (!item.name.equals(BACKSTAGE_PASS_NAME)) {
                         if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            if (!item.name.equals(SULFURAS_NAME)) {
                                 item.quality = item.quality - 1;
                             }
                         }
@@ -55,11 +51,31 @@ class GildedRose {
                         item.quality = 0;
                     }
                 } else {
-                    if (item.quality < 50) {
+                    if (item.quality < ITEM_MAXIMUM_QUALITY) {
                         item.quality = item.quality + 1;
                     }
                 }
             }
         }
     }
+
+    private void updateCommonItemQuality(Item item) {
+        if (item.quality > 0) {
+            if (!item.name.equals(SULFURAS_NAME)) {
+                item.quality = item.quality - 1;
+            }
+        }
+    }
+
+    private void increaseItemQualityIfUnderMaximum(Item item, int increment) {
+        if (item.quality < ITEM_MAXIMUM_QUALITY) {
+            item.quality += increment;
+        }
+    }
+    private void decreaseItemQualityIfOverMinimum(Item item, int decrement) {
+        if (item.quality > ITEM_MINIMUM_QUALITY) {
+            item.quality -= decrement;
+        }
+    }
+
 }
--- src/test/java/com/github/smeny/GildedRoseTest.java
+++ src/test/java/com/github/smeny/GildedRoseTest.java
@@ -2,16 +2,161 @@ package com.github.smeny;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.github.smeny.GildedRose.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 class GildedRoseTest {
 
+    private static final String COMMON_ITEM_NAME = "item";
+
     @Test
-    void foo() {
-        Item[] items = new Item[]{new Item("foo", 0, 0)};
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+    void common_item_with_positive_sell_in_degrades_quality_by_one() {
+        int sellInThirtyDays = 30, quality = 20;
+        Item commonItem = buildCommonItem(sellInThirtyDays, quality);
+        GildedRose shop = buildGildedRoseShop(commonItem);
+
+        shop.updateQuality();
+
+        assertThat(firstItem(shop).sellIn, is(sellInThirtyDays - 1));
+        assertThat(firstItem(shop).quality, is(quality - 1));
     }
 
+
+    @Test
+    void common_item_with_passed_sell_in_degrades_quality_twice_as_fast() {
+        int sellInToday = 0, quality = 20;
+        Item commonItem = buildCommonItem(sellInToday, quality);
+        GildedRose shop = buildGildedRoseShop(commonItem);
+
+        shop.updateQuality();
+
+        assertThat(firstItem(shop).sellIn, is(sellInToday - 1));
+        assertThat(firstItem(shop).quality, is(quality - 2));
+    }
+
+    @Test
+    void common_item_never_has_negative_quality() {
+        int sellInThirtyDays = 30, zeroQuality = 0;
+        Item commonItem = buildCommonItem(sellInThirtyDays, zeroQuality);
+        GildedRose shop = buildGildedRoseShop(commonItem);
+
+        shop.updateQuality();
+
+        assertThat(firstItem(shop).sellIn, is(sellInThirtyDays - 1));
+        assertThat(firstItem(shop).quality, is(zeroQuality));
+    }
+
+    @Test
+    void aged_brie_increases_in_quality_by_one() {
+        int sellInThirtyDays = 30, quality = 20;
+        Item agedBrie = buildAgedBrie(sellInThirtyDays, quality);
+        GildedRose shop = buildGildedRoseShop(agedBrie);
+
+        shop.updateQuality();
+
+        assertThat(firstItem(shop).name, is(AGED_BRIE_NAME));
+        assertThat(firstItem(shop).sellIn, is(sellInThirtyDays - 1));
+        assertThat(firstItem(shop).quality, is(quality + 1));
+    }
+
+    @Test
+    void aged_brie_with_passed_sell_in_increases_in_quality_twice_as_fast() {
+        int sellInToday = 0, quality = 20;
+        Item agedBrie = buildAgedBrie(sellInToday, quality);
+        GildedRose shop = buildGildedRoseShop(agedBrie);
+
+        shop.updateQuality();
+
+        assertThat(firstItem(shop).name, is(AGED_BRIE_NAME));
+        assertThat(firstItem(shop).sellIn, is(sellInToday - 1));
+        assertThat(firstItem(shop).quality, is(quality + 2));
+    }
+
+    @Test
+    void aged_brie_quality_is_never_more_than_fifty() {
+        int sellInThirtyDays = 30, qualityFifty = 50;
+        Item agedBrie = buildAgedBrie(sellInThirtyDays, qualityFifty);
+        GildedRose shop = buildGildedRoseShop(agedBrie);
+
+        shop.updateQuality();
+
+        assertThat(firstItem(shop).name, is(AGED_BRIE_NAME));
+        assertThat(firstItem(shop).sellIn, is(sellInThirtyDays - 1));
+        assertThat(firstItem(shop).quality, is(qualityFifty));
+    }
+
+    @Test
+    void sulfuras_never_decreases_sell_in_or_quality() {
+        int sellInThirtyDays = 30;
+        Item sulfuras = buildSulfuras(sellInThirtyDays);
+        GildedRose shop = buildGildedRoseShop(sulfuras);
+
+        shop.updateQuality();
+
+        assertThat(firstItem(shop).name, is(SULFURAS_NAME));
+        assertThat(firstItem(shop).sellIn, is(sellInThirtyDays));
+        assertThat(firstItem(shop).quality, is(SULFURAS_QUALITY));
+    }
+
+    @Test
+    void backstage_pass_quality_increases_by_two_when_sell_in_ten_days_or_less() {
+        int sellInTenDays = 10, quality = 20;
+        Item backstagePass = buildBackstagePass(sellInTenDays, quality);
+        GildedRose shop = buildGildedRoseShop(backstagePass);
+
+        shop.updateQuality();
+
+        assertThat(firstItem(shop).name, is(BACKSTAGE_PASS_NAME));
+        assertThat(firstItem(shop).sellIn, is(sellInTenDays - 1));
+        assertThat(firstItem(shop).quality, is(quality + 2));
+    }
+    @Test
+    void backstage_pass_quality_increases_by_three_when_sell_in_five_days_or_less() {
+        int sellInFiveDays = 5, quality = 20;
+        Item backstagePass = buildBackstagePass(sellInFiveDays, quality);
+        GildedRose shop = buildGildedRoseShop(backstagePass);
+
+        shop.updateQuality();
+
+        assertThat(firstItem(shop).name, is(BACKSTAGE_PASS_NAME));
+        assertThat(firstItem(shop).sellIn, is(sellInFiveDays - 1));
+        assertThat(firstItem(shop).quality, is(quality + 3));
+    }
+    @Test
+    void backstage_pass_quality_decreases_to_zero_when_sell_in_after_concert() {
+        int sellInToday = 0, quality = 20, qualityZero = 0;
+        Item backstagePass = buildBackstagePass(sellInToday, quality);
+        GildedRose shop = buildGildedRoseShop(backstagePass);
+
+        shop.updateQuality();
+
+        assertThat(firstItem(shop).name, is(BACKSTAGE_PASS_NAME));
+        assertThat(firstItem(shop).sellIn, is(sellInToday - 1));
+        assertThat(firstItem(shop).quality, is(qualityZero));
+    }
+
+    private static GildedRose buildGildedRoseShop(Item... items) {
+        return new GildedRose(items);
+    }
+
+    private static Item buildCommonItem(int sellIn, int quality) {
+        return new Item(COMMON_ITEM_NAME, sellIn, quality);
+    }
+
+    private static Item buildSulfuras(int sellIn) {
+        return new Item(GildedRose.SULFURAS_NAME, sellIn, GildedRose.SULFURAS_QUALITY);
+    }
+
+    private static Item buildAgedBrie(int sellIn, int quality) {
+        return new Item(AGED_BRIE_NAME, sellIn, quality);
+    }
+
+    private static Item buildBackstagePass(int sellIn, int quality) {
+        return new Item(BACKSTAGE_PASS_NAME, sellIn, quality);
+    }
+
+    private static Item firstItem(GildedRose shop) {
+        return shop.items[0];
+    }
 }
